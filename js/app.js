5b1e793e6820c0416bac9afa4f46e1d3add3159d
unknown <ncuti60@gmail.com>
2020-09-18 23:03:26 +0200

    ft:add images
    ft:update js
    
    ft:update css
    
    ft:update index

--- js/app.js
+++ js/app.js
@@ -2,6 +2,54 @@
 function hide() {
     loader.classList.add('slideOutDown')
     loader.style.display = 'none'
-    container.style.visibility = 'visible'
+    container.style.display = 'block'
 }
-setTimeout(() => { hide() }, 3000);
+setTimeout(() => { hide() }, 2000);
+
+
+//slideshow button
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("slide");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "flex";
+}
+
+slideRight.addEventListener('click', () => {
+    plusSlides(1)
+})
+slideLeft.addEventListener('click', () => {
+    plusSlides(-1)
+})
+
+
+// ***************************************
+//slideshow automatic
+var slideIndex = 0
+showSlide();
+
+function showSlide() {
+    var i;
+    var slides = document.getElementsByClassName("slide");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+
+    if (slideIndex > slides.length) { slideIndex = 1 }
+
+    slides[slideIndex - 1].style.display = "flex";
+    setTimeout(showSlide, 5000);
+}
+// ******************************************************
--- js/variable.js
+++ js/variable.js
@@ -1,7 +1,13 @@
-const variables = {
-    loader:document.querySelector('.loading'),
-    container:document.querySelector('.container')
-}
-
 const container = document.querySelector('.container')
 const loader = document.querySelector('.loading')
+const slides = document.querySelectorAll('.slide')
+const navbar = document.querySelector('.navbar')
+
+const slideshow = document.querySelector('#slideshow')
+const slideBtn = document.querySelector('.slide_btn')
+const slideLeft = document.querySelector('.slide_left_btn')
+const slideRight = document.querySelector('.slide_right_btn')
+
+window.addEventListener('scroll', function () {
+    scroll.textContent = window.pageYOffset + 'px';
+});
